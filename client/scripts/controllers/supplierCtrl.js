5145c056bf6b1d2fe27b413b8b2d3ff139e30a3b
Yuan Zhencai <zhencai.yuan@sunlights.cc>
2015-04-14 15:20:25 +0800

    suppler dialog

--- client/scripts/controllers/supplierCtrl.js
+++ client/scripts/controllers/supplierCtrl.js
@@ -1,19 +1,21 @@
-(function() {
+(function () {
   var SupplierCtrl;
 
-  SupplierCtrl = (function() {
-    function SupplierCtrl($scope, $rootScope, $log, $location, SupplierService) {
+  SupplierCtrl = (function () {
+    function SupplierCtrl($scope, $rootScope, $log, $location, toaster, ngDialog, SupplierService) {
       this.$scope = $scope;
       this.$rootScope = $rootScope;
       this.$log = $log;
       this.$location = $location;
+      this.toaster = toaster;
+      this.ngDialog = ngDialog;
       this.SupplierService = SupplierService;
       this.$log.debug("constructing SupplierCtrl");
       this.suppliers = [];
-      this.supplier = this.$rootScope.supplier || {};
-      this.$rootScope.supplier = {};
+      this.supplier = this.$scope.supplier || {};
       this.pager = {};
-      $scope.gridOptions = {
+      this.dialog = {};
+      this.$scope.gridOptions = {
         data: 'pager.list',
         enablePaging: true,
         showFooter: true,
@@ -59,60 +61,97 @@
       };
     }
 
-    SupplierCtrl.prototype.findSuppliers = function() {
+    SupplierCtrl.prototype.findSuppliers = function () {
       this.$log.debug("findSuppliers()");
       this.pager.pageSize = this.$scope.gridOptions.pagingOptions.pageSize;
       this.pager.pageNum = this.$scope.gridOptions.pagingOptions.currentPage;
-      return this.SupplierService.findSuppliers(this.pager).then((function(_this) {
-        return function(data) {
+      return this.SupplierService.findSuppliers(this.pager).then((function (_this) {
+        return function (data) {
           _this.$log.debug("Promise returned " + data.value.list.length + " Suppliers");
           return _this.$scope.pager = data.value;
         };
-      })(this), (function(_this) {
-        return function(error) {
+      })(this), (function (_this) {
+        return function (error) {
           _this.$log.error("Unable to get Suppliers: " + error);
           return _this.error = error;
         };
       })(this));
     };
 
-    SupplierCtrl.prototype.saveSupplier = function() {
+    SupplierCtrl.prototype.saveSupplier = function () {
       this.$log.debug("saveSupplier()");
-      return this.SupplierService.saveSupplier(this.supplier).then((function(_this) {
-        return function(data) {
-          _this.$log.debug("save supplier successfully");
-          return _this.$location.path("/dashboard/supplier");
+      this.supplier = angular.copy(this.$scope.supplier);
+      return this.SupplierService.saveSupplier(this.supplier).then((function (_this) {
+        return function (data) {
+          _this.findSuppliers();
+          _this.dialog.close();
+          _this.toaster.pop('success', data.message.summary, data.message.detail);
+          return _this.$log.debug("save supplier successfully");
         };
-      })(this), (function(_this) {
-        return function(error) {
-          _this.$log.error("Unable to save supplier: " + error);
-          return _this.error = error;
+      })(this), (function (_this) {
+        return function (error) {
+          _this.$scope.error = error;
+          return _this.$log.error("Unable to save supplier: " + error);
         };
       })(this));
     };
 
-    SupplierCtrl.prototype.createSupplier = function() {
-      this.$log.debug("createSupplier()");
-      return this.$location.path("/dashboard/supplier/save");
+    SupplierCtrl.prototype.createSupplier = function () {
+      this.$scope.error = null;
+      this.$scope.supplier = {};
+      this.dialog = this.ngDialog.open({
+        template: 'supplierSaveDialog',
+        className: 'ngdialog-theme-plain custom-width',
+        scope: this.$scope,
+        preCloseCallback: (function (_this) {
+          return function (value) {
+            _this.$log.debug("preCloseCallback()");
+            if ('confirm' === value) {
+              _this.saveSupplier();
+              return false;
+            } else {
+              return true;
+            }
+          };
+        })(this)
+      });
+      return this.$log.debug("createSupplier()");
     };
 
-    SupplierCtrl.prototype.updateSupplier = function(row) {
-      this.$log.debug("updateSupplier()");
-      this.supplier = row.entity;
-      this.$rootScope.supplier = this.supplier;
-      return this.$location.path("/dashboard/supplier/save");
+    SupplierCtrl.prototype.updateSupplier = function (supplier) {
+      this.$scope.error = null;
+      this.$scope.supplier = angular.copy(supplier);
+      this.dialog = this.ngDialog.open({
+        template: 'supplierSaveDialog',
+        className: 'ngdialog-theme-plain custom-width',
+        scope: this.$scope,
+        preCloseCallback: (function (_this) {
+          return function (value) {
+            _this.$log.debug("preCloseCallback()");
+            if ('confirm' === value) {
+              _this.saveSupplier();
+              return false;
+            } else {
+              return true;
+            }
+          };
+        })(this)
+      });
+      return this.$log.debug("updateSupplier()");
     };
 
-    SupplierCtrl.prototype.deleteSupplier = function(row) {
+    SupplierCtrl.prototype.deleteSupplier = function (supplier) {
       this.$log.debug("deleteSupplier()");
-      return this.SupplierService.deleteSupplier(row.entity).then((function(_this) {
-        return function(data) {
+      return this.SupplierService.deleteSupplier(supplier).then((function (_this) {
+        return function (data) {
           _this.$log.debug("successfully delete supplier");
+          _this.toaster.pop('success', data.message.summary, data.message.detail);
           return _this.findSuppliers();
         };
-      })(this), (function(_this) {
-        return function(error) {
+      })(this), (function (_this) {
+        return function (error) {
           _this.$log.error("Unable to delete supplier: " + error);
+          _this.toaster.pop('error', error.message.summary, error.message.detail);
           return _this.error = error;
         };
       })(this));
@@ -122,7 +161,7 @@
 
   })();
 
-    angular.module('sbAdminApp').controller('SupplierCtrl', SupplierCtrl);
+  angular.module('sbAdminApp').controller('SupplierCtrl', SupplierCtrl);
 
 }).call(this);
 
